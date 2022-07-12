8df7c78f76901695ca3c7e1c27d5fc4149d90045
joseph Estevez <joseph.estevez@protonmail.com>
2022-07-12 19:07:53 -0400

    feat styles: reset css

--- src/styles/global.styled.tsx
+++ src/styles/global.styled.tsx
@@ -6,9 +6,20 @@ const GlobalStyle = createGlobalStyle`
     background-color: ${(props) => props.theme.selection_background};
   }
 
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
   body {
     color: ${(props) => props.theme.foreground};
     background-color: ${(props) => props.theme.background};
+  }
+
+  ul, li {
+    list-style: none;
+    text-decoration: none;
   }
 `
 export default GlobalStyle
