8f6a09555ec7f81c35b6cf5ce42442bb38732103
joseph Estevez <joseph.estevez@protonmail.com>
2022-07-12 18:06:12 -0400

    format

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import reactPlugin from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import reactPlugin from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [tsconfigPaths(), reactPlugin()],
   server: {
-    port: 4444,
-  },
-});
+    port: 4444
+  }
+})
