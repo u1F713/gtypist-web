2ed2e4cc6a3d503320ab95bbe36a7033f019373e
joseph Estevez <joseph.estevez@protonmail.com>
2022-07-22 23:25:44 -0400

    feat: add menu selection

--- src/layouts/navigation/index.tsx
+++ src/layouts/navigation/index.tsx
@@ -1,7 +1,25 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
+import { useAppDispatch } from '~/store/app.selectors'
+import { actions as menuActions } from '@feat/navigation/menu'
 import NavigationList from './components/list'
+import { useSeries } from '~/features/series/state/useSeries'
 
 const NavigationMenu: FC = () => {
+  const dispatch = useAppDispatch()
+  const { series } = useSeries()
+
+  useEffect(() => {
+    dispatch(
+      menuActions.setMenu({
+        menu: {
+          selected: `The following ${series.ids.length} lesson series are available`,
+          description: '',
+          options: []
+        }
+      })
+    )
+  }, [])
+
   return (
     <section>
       <NavigationList />
