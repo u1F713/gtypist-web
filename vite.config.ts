78fdde563f6cbb0df6bd6d2740c918d58df78f3c
Joseph Estevez <joseph.estevez@protonmail.com>
2022-08-20 21:14:24 -0400

    chore: optimize imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import reactPlugin from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [tsconfigPaths(), reactPlugin()],
