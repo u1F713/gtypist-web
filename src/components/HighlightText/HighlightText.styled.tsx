3296bbb5837eb0e74260ebaa1ea919e54e40983f
Joseph Estevez <joseph.estevez@protonmail.com>
2022-08-22 21:24:13 -0400

    fix css: add white-space

--- src/components/HighlightText/HighlightText.styled.tsx
+++ src/components/HighlightText/HighlightText.styled.tsx
@@ -2,5 +2,6 @@ import styled from 'styled-components'
 
 export const Container = styled.span`
   color: ${(props) => props.theme.background};
+  white-space: break-spaces;
   background-color: #${(props) => props.theme.colors[5].code};
 `
